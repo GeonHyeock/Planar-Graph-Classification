d5dd2c4474109f0316d24dfd8de8d98cd1448478
GeonHyeock <gunheock@gmail.com>
2023-12-17 02:14:43 +0900

    Feat : data viewer predict loss

--- src/data_viewer.py
+++ src/data_viewer.py
@@ -8,12 +8,37 @@ from matplotlib import pyplot as plt
 from glob import glob
 
 
+def my_tab1(data_type, df, idx):
+    col1, col2 = st.columns(2)
+    with col1:
+        graph_visual(df.iloc[idx])
+    with col2:
+        st.dataframe(df.iloc[idx].astype(str), width=int(1e3))
+        if data_type == "test":
+            PF = df.iloc[idx]["colors"] == (df.iloc[idx]["predict"] + 1)
+            if PF == True:
+                st.write("예측 성공")
+            else:
+                st.write("예측 실패")
+
+
+def pred_df(data_path, df):
+    a, b = data_path.split("/")
+    folder_path = os.path.join(a, "result", b)
+    paths = glob(folder_path + "/*")
+    predict_path = st.selectbox("result를 선택해주세요.", [p.split("/")[-1] for p in paths])
+    predict_df = pd.read_csv(os.path.join(folder_path, predict_path))
+    predict_df = pd.merge(df, predict_df, how="inner").sort_values(
+        "loss", ascending=False
+    )
+    return predict_df
+
+
 def graph_visual(data):
     G = nx.from_edgelist(pd.read_csv(data.data_path).values.astype(int))
     fig = plt.figure(figsize=(5, 5))
     nx.draw(G)
     st.pyplot(fig)
-    st.dataframe(data.astype(str))
 
 
 def gh_plot(df):
@@ -24,14 +49,8 @@ def gh_plot(df):
         plt.xlim(0, max(df[col]) * 1.1)
         st.pyplot(fig)
 
-    sns.set(font_scale=1)
-    fig = plt.figure(figsize=(10, 10))
-    sns.scatterplot(data=df, x="edge_number", y="colors")
-    st.pyplot(fig)
 
-
-def main():
-    st.title("Chromatic Number of Graphs")
+def slidebar():
     with st.sidebar:
         data_path = st.selectbox("data version을 선택해주세요.", glob("data/version*"))
         df = pd.read_csv(os.path.join(data_path, "label.csv"))
@@ -40,11 +59,20 @@ def main():
             df = df[df["type"] == data_type]
 
         idx = st.slider("data를 선택해주세요.", 0, len(df) - 1, 0, 1)
+        return df, idx, data_path, data_type
 
-    with st.expander("Graph"):
-        graph_visual(df.iloc[idx])
 
-    with st.expander("DATA EDA"):
+def main():
+    st.title("Chromatic Number of Graphs")
+
+    tab1, tab2 = st.tabs(["Data_View", "EDA"])
+    df, idx, data_path, data_type = slidebar()
+    if data_type == "test":
+        df = pred_df(data_path, df)
+
+    with tab1:
+        my_tab1(data_type, df, idx)
+    with tab2:
         gh_plot(df)
 
 
