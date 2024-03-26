8a938878571ec9ae39b8ba3796a50bc4e2e5571a
GeonHyeock <gunheock@gmail.com>
2024-03-26 16:28:04 +0900

    Feat : data sample

--- src/make_data.py
+++ src/make_data.py
@@ -32,36 +32,28 @@ def create_folder(args):
             sys.exit()
 
 
-def make_label_graph(args, label, PG, weight_funtion=lambda x: x * (x - 1)):
+def make_label_graph(args, label, PG, weight_funtion=lambda x: np.log2(x)):
+
+    sample_node, G = [], nx.Graph([(1, 2), (3, 4)])
+    while not (args.min_node <= G.number_of_nodes() and len(sample_node) <= args.max_node and nx.is_connected(G)):
+        start_node = int(np.random.choice(PG.nodes, 1))
+        depth = np.random.choice(range(1, 15), 1)
+        sample_node = set(sum(map(list, nx.dfs_edges(PG, start_node, depth)), []))
+        edges = PG.subgraph(sample_node).edges
+        sub_edge = np.random.permutation(edges)[: (int(len(edges) * np.random.uniform(0.2, 1)))]
+        G = nx.from_edgelist(sub_edge)
+    G = nx.from_numpy_array(nx.to_numpy_array(G))
+    assert nx.is_connected(G), "연결 그래프"
 
     if label == "is_PlanarGraph":
-        sample_node = []
-        while args.min_node > len(sample_node) or len(sample_node) > args.max_node:
-            start_node = int(np.random.choice(PG.nodes, 1))
-            depth = np.random.choice(range(1, 15), 1)
-            sample_node = sum([list(i) for i in nx.dfs_edges(PG, start_node, depth)], [])
-        G = PG.subgraph(sample_node)
-        G = nx.from_numpy_array(nx.to_numpy_array(G))
-        assert nx.is_connected(G), "연결 그래프"
         return G
 
     elif label == "is_not_PlanarGraph":
-        n_range = range(args.min_node, args.max_node + 1)
-        is_conected, count = False, 0
-        while not is_conected:
-            if count // 1000 == 0:
-                n = random.choices(n_range, weights=map(weight_funtion, n_range))[0]
-                m_range = range(n - 1, (3 * n - 6) + 1)
-                m = random.choices(m_range)[0]
-            G = nx.gnm_random_graph(n, m)
-            is_conected = nx.is_connected(G)
-            count += 1
-
         my_sub = random.choice(["K5", "K3_3", "K5&K3_3"])
         if "K5" in my_sub:
             sample_node = np.random.choice(G.nodes, size=5, replace=False)
-            for e in combinations(sample_node, 2):
-                G.add_edge(*e)
+            for a, b in combinations(sample_node, 2):
+                G.add_edge(a, b)
         if "K3_3" in my_sub:
             sample_node = np.random.choice(G.nodes, size=6, replace=False)
             A, B = np.split(sample_node, 2)
@@ -89,6 +81,7 @@ def make_graph(args):
         df["data_path"].append(data_path)
         df["label_name"].append(args.labelname2label[label])
         df["node_number"].append(G.number_of_nodes())
+        df["edge_number"].append(G.number_of_edges())
 
     df = pd.DataFrame(df)
     data_type = ["train"] * 8 + ["valid"] * 1 + ["test"] * 1
@@ -114,9 +107,9 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--version", default=6, help="data_version")
-    parser.add_argument("--min_node", default=10, help="그래프 노드의 최소 개수")
-    parser.add_argument("--max_node", default=100, help="그래프 노드의 최대 개수")
-    parser.add_argument("--N", default=25000, help="Sample_size")
+    parser.add_argument("--min_node", default=16, help="그래프 노드의 최소 개수")
+    parser.add_argument("--max_node", default=1024, help="그래프 노드의 최대 개수")
+    parser.add_argument("--N", default=5000, help="Sample_size")
     parser.add_argument("--label_name", nargs="+", default=["is_not_PlanarGraph", "is_PlanarGraph"], help="데이터 라벨")
     parser.add_argument("--PlanarGraph", default="data/planar_embedding1000000.pg", help="평면그래프")
     args = parser.parse_args()
