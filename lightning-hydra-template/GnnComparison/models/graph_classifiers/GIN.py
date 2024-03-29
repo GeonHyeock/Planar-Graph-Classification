8795609e5e3aa840a2707f8e3981cd7220d87868
GeonHyeock <gunheock@gmail.com>
2024-03-29 23:34:35 +0900

    ADD : GIN

--- lightning-hydra-template/GnnComparison/models/graph_classifiers/GIN.py
+++ lightning-hydra-template/GnnComparison/models/graph_classifiers/GIN.py
@@ -16,41 +16,64 @@
 #
 import torch
 import torch.nn.functional as F
-from torch.nn import BatchNorm1d
-from torch.nn import Sequential, Linear, ReLU
+from torch.nn import BatchNorm1d, Embedding
+from torch.nn import Sequential, Linear, ReLU, Softmax
+from torch_geometric.utils import dense_to_sparse
 from torch_geometric.nn import GINConv, global_add_pool, global_mean_pool
 
 
 class GIN(torch.nn.Module):
 
-    def __init__(self, dim_features, dim_target, config):
+    def __init__(
+        self,
+        node_number,
+        embedding_size,
+        dim_target,
+        hidden_units,
+        train_eps,
+        aggregation,
+        dropout,
+    ):
         super(GIN, self).__init__()
 
-        self.config = config
-        self.dropout = config['dropout']
-        self.embeddings_dim = [config['hidden_units'][0]] + config['hidden_units']
+        self.dropout = dropout
+        self.emb = Embedding(node_number, embedding_size, padding_idx=0)
+        self.embeddings_dim = [hidden_units[0]] + hidden_units
         self.no_layers = len(self.embeddings_dim)
         self.first_h = []
         self.nns = []
         self.convs = []
         self.linears = []
 
-        train_eps = config['train_eps']
-        if config['aggregation'] == 'sum':
+        if aggregation == "sum":
             self.pooling = global_add_pool
-        elif config['aggregation'] == 'mean':
+        elif aggregation == "mean":
             self.pooling = global_mean_pool
 
         for layer, out_emb_dim in enumerate(self.embeddings_dim):
 
             if layer == 0:
-                self.first_h = Sequential(Linear(dim_features, out_emb_dim), BatchNorm1d(out_emb_dim), ReLU(),
-                                    Linear(out_emb_dim, out_emb_dim), BatchNorm1d(out_emb_dim), ReLU())
+                self.first_h = Sequential(
+                    Linear(embedding_size, out_emb_dim),
+                    BatchNorm1d(out_emb_dim),
+                    ReLU(),
+                    Linear(out_emb_dim, out_emb_dim),
+                    BatchNorm1d(out_emb_dim),
+                    ReLU(),
+                )
                 self.linears.append(Linear(out_emb_dim, dim_target))
             else:
-                input_emb_dim = self.embeddings_dim[layer-1]
-                self.nns.append(Sequential(Linear(input_emb_dim, out_emb_dim), BatchNorm1d(out_emb_dim), ReLU(),
-                                      Linear(out_emb_dim, out_emb_dim), BatchNorm1d(out_emb_dim), ReLU()))
+                input_emb_dim = self.embeddings_dim[layer - 1]
+                self.nns.append(
+                    Sequential(
+                        Linear(input_emb_dim, out_emb_dim),
+                        BatchNorm1d(out_emb_dim),
+                        ReLU(),
+                        Linear(out_emb_dim, out_emb_dim),
+                        BatchNorm1d(out_emb_dim),
+                        ReLU(),
+                    )
+                )
                 self.convs.append(GINConv(self.nns[-1], train_eps=train_eps))  # Eq. 4.2
 
                 self.linears.append(Linear(out_emb_dim, dim_target))
@@ -58,9 +81,18 @@ class GIN(torch.nn.Module):
         self.nns = torch.nn.ModuleList(self.nns)
         self.convs = torch.nn.ModuleList(self.convs)
         self.linears = torch.nn.ModuleList(self.linears)  # has got one more for initial input
+        self.soft_max = Softmax(dim=-1)
 
     def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
+        if data.shape[0] > 1:
+            x = torch.stack([self.online_forward(d) for d in data]).squeeze()
+        else:
+            x = self.online_forward(data.squeeze())
+        return self.softmax(x)
+
+    def online_foward(self, data, batch=None):
+        x = self.emb(torch.sum(data, dim=-1).type(torch.long))
+        edge_index, _ = dense_to_sparse(data)
 
         out = 0
 
@@ -70,7 +102,7 @@ class GIN(torch.nn.Module):
                 out += F.dropout(self.pooling(self.linears[layer](x), batch), p=self.dropout)
             else:
                 # Layer l ("convolution" layer)
-                x = self.convs[layer-1](x, edge_index)
+                x = self.convs[layer - 1](x, edge_index)
                 out += F.dropout(self.linears[layer](self.pooling(x, batch)), p=self.dropout, training=self.training)
 
         return out
